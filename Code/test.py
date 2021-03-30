9c7778afb99951cae8dda8b7d7a09b3e1326a4d9
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2021-03-30 17:39:29 -0400

    Create test.py

---
+++ Code/test.py
@@ -0,0 +1,59 @@
+import random
+import time
+import picamera
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.OUT)
+pwm=GPIO.PWM(11, 50)
+pwm.start(0)
+
+
+
+def SetAngle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(11, True)
+    pwm.ChangeDutyCycle(duty)
+    sleep(1)
+    GPIO.output(11, False)
+    pwm.ChangeDutyCycle(0)
+
+def beginRecording():
+    camera = picamera.PiCamera()    # Setting up the camera
+    camera.start_preview()      # You will see a preview window while recording
+    camera.start_recording('video.h264') # Video will be saved at desktop
+    
+
+def setAngle(angle):
+    duty = angle / 18 + 2
+    GPIO.output(11, True)
+    pwm.ChangeDutyCycle(duty)
+    sleep(1)
+    GPIO.output(11, False)
+    pwm.ChangeDutyCycle(0)
+
+def getAcceleration():
+    return random.randint(1, 10000)
+
+def fileName():
+    name=time.asctime()
+    return name
+
+
+#file1 = open(r"D:\Text\accelerationinfo.txt","a+")
+#file1.write(fileName()+"\n")
+#for x in range(6):
+#    z=str(getAcceleration())
+#    file1.write("Acceleration: "+z + "\n")
+    
+#file1.close()
+beginRecording()
+sleep(3)
+setAngle(35)
+sleep(10)
+camera.stop_recording()
+camera.stop_preview()
+pwm.stop()
+GPIO.cleanup()
