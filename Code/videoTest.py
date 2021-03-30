a99f6f58b501632e8dd2fe97a2e5b3960d9b6d11
vwellmo57 <55098291+vwellmo57@users.noreply.github.com>
2021-03-30 17:59:29 -0400

    Update videoTest.py

--- Code/videoTest.py
+++ Code/videoTest.py
@@ -3,13 +3,9 @@ import picamera
 import RPi.GPIO as GPIO
 from time import sleep
 
-print("Start")
 camera = picamera.PiCamera()    # Setting up the camera
-camera.start_preview()      # You will see a preview window while recording
-camera.start_recording('video2.mp4') # Video will be saved at desktop
-
+   
+camera.start_recording('video2.h264') # Video will be saved at desktop
 
 sleep(3)
 camera.stop_recording()
-camera.stop_preview() 
-print("Stop")
