92bf0b0495285b3992a30a7b1cd875e28388e67b
Philip Breen <pbreen40@charlottesvilleschools.org>
2021-04-21 15:25:50 +0100

    indent

--- Code/app.py
+++ Code/app.py
@@ -1,3 +1,7 @@
+
+
+
+
 from flask import Flask, render_template, request
 from time import sleep
 import RPi.GPIO as GPIO
@@ -17,7 +21,6 @@ acceleration = Adafruit_LSM303.LSM303()
 app = Flask(__name__)
 def beginRecording():
     camera.start_recording(fileName() +'.h264') # Video will be saved at desktop
-    
 
 def setAngle(angle):
     duty = angle / 18 + 2
@@ -44,33 +47,29 @@ def fileName():
 @app.route("/", methods=["GET","POST"])
 def index():
 	print(request.method)
-    		if request.method == 'POST':
-        		if request.form.get('button1') == 'button1':
-   	        		beginRecording()
-            			sleep(3)
-          			setAngle(35)
-            			file1 = open(r"accelerationinfo.txt","a+")
-            			file1.write(fileName()+"\n")
-            			for x in range(300):
-                			accelx,accely,accelz = getAcceleration()
-                			accelx = str(accelx)
-                			accely = str(accely)
-                			accelz = str(accelz)
-                			file1.write("X Axis: " + accelx + "  Y Axis: " + accely + "  Z Axis: " + accelz + "\n")
-                			sleep(.033333333333333)
-                			file1.write("\n")
-                			camera.stop_recording()
-                			pwm.stop()
-                			GPIO.cleanup()
-      
-      		else:
-            		return render_template("index.html")
+	if request.method == 'POST':
+		if request.form.get('button1') == 'button1':
+			beginRecording()
+			sleep(3)
+			setAngle(35)
+			file1 = open(r"accelerationinfo.txt","a+")
+			file1.write(fileName()+"\n")
+			for x in range(300):
+				accelx,accely,accelz = getAcceleration()
+				accelx = str(accelx)
+				accely = str(accely)
+				accelz = str(accelz)
+				file1.write("X Axis: " + accelx + "  Y Axis: " + accely + "  Z Axis: " + accelz + "\n")
+				sleep(.033333333333333)
+				file1.write("\n")
+				camera.stop_recording()
+				pwm.stop()
+				GPIO.cleanup()
+		else:
+			return render_template("index.html")
 
 	elif request.method == 'GET':
-         
         	print("No Post Back Call")
-    	return render_template("index.html")
-
-
+	return render_template("index.html")
 if __name__ == "__main__":
-     app.run(host="0.0.0.0", port=80)
+	app.run(host="0.0.0.0", port=80)
